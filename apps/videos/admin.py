a3e95b62dd6a0f3d1171c5a1eae8df409211a1da
Wojciech Dudek <wdudek82@gmail.com>
2017-03-16 18:26:45 +0100

    Set admin for Video model

--- apps/videos/admin.py
+++ apps/videos/admin.py
@@ -6,3 +6,7 @@ from .models import Video
 class VideoAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'embed_code', 'created', 'changed', 'modified']
     list_display_links = ['title']
+    readonly_fields = ['created', 'changed', 'modified']
+    search_fields = ['title', 'embeded_code']
+    list_filter = ['created', 'modified']
+
