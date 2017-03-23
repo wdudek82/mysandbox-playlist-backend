21f6bcd30e83fc8030f2990f81d135628d15972a
Wojciech Dudek <wdudek82@gmail.com>
2017-03-24 00:16:53 +0100

    Added Lecture model, Lecture inline to CourseAdmin, LectureAdmin

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Lecture
 
 
+class LectureInline(admin.TabularInline):
+    model = Lecture
+    extra = 1
+    exclude = ('created', 'modified')
+
+
+# TODO: same as in models - maybe better would be to use abstract class to reduce code repetition
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'title', 'slug', 'description', 'price', 'created', 'changed', 'modified']
     readonly_fields = ['created', 'modified']
     list_display_link = ['title']
     list_filter = ['user', 'price', 'created', 'modified']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [
+        LectureInline,
+    ]
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'slug', 'description', 'course', 'video',  'created', 'modified', 'changed']
+    readonly_fields = ['created', 'modified']
+    list_display_links = ['title']
+    list_filter = ['course', 'video', 'created', 'modified']
     search_fields = ['title', 'description']
     prepopulated_fields = {'slug': ('title',)}
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+from apps.videos.models import Video
 from apps.utils.create_unique_slug import create_unique_slug
 
 
@@ -22,7 +23,24 @@ class Course(Timestamped):
         return reverse('course:detail', kwargs={'slug': self.slug})
 
 
+# TODO: I guess it's too much repetition - I'll try to use abstract class for videos, courses, and lectured
+# TODO: create app abstrac_classes (?)
+class Lecture(Timestamped):
+    course = models.ForeignKey(Course, null=True, blank=True, on_delete=models.SET_NULL)
+    video = models.ForeignKey(Video, null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(blank=True, help_text='Some help text for slug field')
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('lecture:detail', kwargs={'slug': self.slug})
+
+
 @receiver(signal=pre_save, sender=Course)
+@receiver(signal=pre_save, sender=Lecture)
 def pre_save_course_receiver(sender, instance, *args, **kwargs):
     queryset = Course.objects.filter(slug=instance.slug)
     if not instance.slug or queryset.count() > 1:
