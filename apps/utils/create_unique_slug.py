62d9b90a4408521338419c9d37bb3709daf67230
Wojciech Dudek <wdudek82@gmail.com>
2017-03-23 22:47:41 +0100

    Added templates for Course delete, changed other templates

--- apps/utils/create_unique_slug.py
+++ apps/utils/create_unique_slug.py
@@ -7,10 +7,11 @@ def create_unique_slug(instance, new_slug=None):
     else:
         slug = new_slug
 
-    print(slug)
     Klass = instance.__class__
     queryset = Klass.objects.filter(slug=slug).order_by('-id')
     if queryset:
+        # TODO: Bleh.. there is still a nasty bug here - it won't work with primary slug, and if that primary has
+        # hyphen, it will be split in incorrect way - need fix!
         created_slug = slug.rsplit('-')[0] + '-{id_}'.format(id_=queryset.first().id)
         return create_unique_slug(instance, new_slug=created_slug)
     return slug
